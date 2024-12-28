cb72647ca5357a565587cd443d4a76f6f1baa22a
Ardhweb <sun.projectsdev@gmail.com>
2024-12-28 22:34:09 +0530

    Accounts Pages Added and Some Design

---
+++ accounts/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+from django.contrib import admin
+app_name='accounts'
+urlpatterns = [
+    # # change password urls
+    # path('password_change/',auth_views.PasswordChangeView.as_view(),name='password_change'),
+    # path('password_change/done/',auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # #reset password
+    # path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    # path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+    
+
+    path('login/', views.user_login, name='account_login'),
+    path('logout/', views.logout, name='logout'),
+    #reg
+    path('register/', views.register, name='register'),
+    path('signup', views.signup, name='signup'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django.shortcuts import render
+from django.http import request
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout                                         
+from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
+from .forms import LoginForm, RegistrationForm
+from django.contrib.auth.models import AnonymousUser
+from django.http.response import HttpResponseRedirect
+from django.template.context import RequestContext
+from django.urls import reverse
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                        username=cd['username'],
+                                        password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('club:dashboard')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout(request):
+    logout(request)
+    return HttpResponseRedirect(request,'log_out.html',context_instance = RequestContext(request))
+    
+
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = RegistrationForm(request.POST)
+        if user_form.is_valid ():
+            new_user = user_form.save(commit=False)
+
+            new_user.set_password(
+                                user_form.cleaned_data['password'])
+
+            new_user.save()
+
+            return render(request,'accounts/register_done.html',{'new_user': new_user})
+    else:
+        user_form = RegistrationForm()
+    return render(request,'accounts/register.html',{'user_form': user_form})
+            
+
+#signup for new customers that don't have account
+def signup(request):
+    return render(request, 'accounts/signup.html')
+
+
+
+
+
+#Password
