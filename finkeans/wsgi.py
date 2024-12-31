bb89e460478b1f0593fc078ff3f1be8ec2faa05e
Ardhweb <sun.projectsdev@gmail.com>
2024-12-31 21:47:23 +0530

    eeee

--- finkeans/wsgi.py
+++ finkeans/wsgi.py
@@ -1,27 +1,5 @@
-"""
-WSGI config for finkeans project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finkeans.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finekeans.settings')
 application = get_wsgi_application()
-'''
-import sys
-from django.core.wsgi import get_wsgi_application
-# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/finekeans/finekeans/'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'finekeans.settings'
-
-application = get_wsgi_application()'''
