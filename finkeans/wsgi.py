3840cfe1f9a766a21d92edc733c8d8eb806e7a0d
Ardhweb <sun.projectsdev@gmail.com>
2025-01-01 14:48:49 +0530

    Update wsgi.py

--- finkeans/wsgi.py
+++ finkeans/wsgi.py
@@ -1,5 +1,11 @@
 import os
 from django.core.wsgi import get_wsgi_application
+import sys
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/jusctod/finekeans'
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finekeans.settings')
 application = get_wsgi_application()
