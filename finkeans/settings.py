97769faadd3ed24e6fc2774868bbfd81589898c2
Ardhweb <sun.projectsdev@gmail.com>
2025-01-01 17:45:12 +0530

    uo

--- finkeans/settings.py
+++ finkeans/settings.py
@@ -12,21 +12,23 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
+import ast
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ae8jhc19xskho6&6*qfwb#m16r+amteg+#8*)*q3-74x95l-^p'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-#ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1]").split(",")
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ast.literal_eval(os.getenv('ALLOWED_HOSTS', "[]"))
 
 
 # Application definition
